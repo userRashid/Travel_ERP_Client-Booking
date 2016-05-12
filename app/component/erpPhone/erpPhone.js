1fab2fbc4a584d66d1ad09c81449a54b7179be75
Manpreet <manpreet3289@gmail.com>
2016-05-13 00:21:50 +0530

    validation on phone number (must begin with 7 or 8)

--- app/component/erpPhone/erpPhone.js
+++ app/component/erpPhone/erpPhone.js
@@ -11,12 +11,18 @@ angular.module('sbAdminApp').directive('erpPhone',function($compile){
     element.html('').append($compile(renderHTML())($scope));
   }
   function controller($scope){
+        var phoneNumbr=/^[7|8]\d*/;
+        $scope.$watch('data',function(_data){
+            $scope.erorr=""
+            if(!phoneNumbr.test(_data.model)&&_data.model) $scope.erorr="Phone Number must begin with either 7 or 8";
+        },true);
   }
   function renderHTML(){
     var html = '';
         html +='<div class="input-group">' +
                 '<span ng-if="data.code" class="input-group-addon">{{data.code}}</span>' +
-                '<input type="text" ng-model="data.model" class="form-control">' +
+                '<span style="color:red;">{{erorr}}</span>' +
+                '<input type="text" ng-model="data.model" class="form-control" >' +
             '</div>';
     return html;
   }
