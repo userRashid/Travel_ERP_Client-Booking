46f4a69e065004e0b44e76f33a1a2e1c1395435c
Manpreet <manpreet3289@gmail.com>
2016-05-14 21:36:27 +0530

    done phone validation(must begin with 7,8,9 and must be of 10 digits)

--- app/component/erpPhone/erpPhone.js
+++ app/component/erpPhone/erpPhone.js
@@ -11,19 +11,17 @@ angular.module('sbAdminApp').directive('erpPhone',function($compile){
     element.html('').append($compile(renderHTML())($scope));
   }
   function controller($scope){
-        var phoneNumbr = /^[7|8|9]\d*/;
-        $scope.$watch('data',function(_data){
-            $scope.erorr = '';
-            if(!phoneNumbr.test(_data.model)&&_data.model) $scope.erorr="Phone Number must begin with either 7 or 8";
-        },true);
+         $scope.phoneNumberPattern = /^[7|8|9]\d*/;
   }
+
   function renderHTML(){
     var html = '';
-        html +='<div class="input-group">' +
-                '<span ng-if="data.code" class="input-group-addon">{{data.code}}</span>' +
-                '<span style="color:red;">{{erorr}}</span>' +
-                '<input type="text" ng-model="data.model" class="form-control" >' +
-            '</div>';
+        html +='<span style="color:red;" ng-show="myerpPhone.myInput.$error.pattern">Phone number must begin with either 7,8 or 9 </span>' +
+               '<span style="color:red;" ng-show="myerpPhone.myInput.$error.maxlength">Phone number must of 10 digits</span>' +
+                '<div class="input-group" ng-form name="myerpPhone">' +
+                  '<span ng-if="data.code" class="input-group-addon">{{data.code}}</span>' +
+                  '<input name="myInput" type="text" ng-model="data.model" class="form-control"  ng-pattern="phoneNumberPattern" ng-maxlength="10" >' +
+                '</div>';
     return html;
   }
 });
