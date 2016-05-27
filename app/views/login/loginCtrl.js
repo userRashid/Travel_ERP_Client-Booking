6ad189a353d044da0d78b9b16656259c85c5ef43
Manpreet <manpreet3289@gmail.com>
2016-05-28 00:02:19 +0530

    revert chnages

--- app/views/login/loginCtrl.js
+++ app/views/login/loginCtrl.js
@@ -1,9 +1,9 @@
 angular.module('sbAdminApp').controller('Login', function ($scope,Navigation,Authenticate,API) {
     $scope.Login = function(data){
-      /* API.post('employee/login',data).then(function(response){
+       API.post('employee/login',data).then(function(response){
             Authenticate.doLogin('',response.data.erp_emp_id,response.data.erp_emp_name);
-        });*/
-         Authenticate.doLogin('',9,'Rashid');
+        });
+      //   Authenticate.doLogin('',9,'Rashid');
     };
     $scope.Navigation = Navigation;
     $scope.Authenticate = Authenticate;
