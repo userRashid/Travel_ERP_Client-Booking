265d44bb6bb2ac999191a0308a5555dbcbe93766
Rashid <user.rashid@gmail.com>
2016-05-31 23:56:05 +0530

    - fix some issue.

--- app/config/config.js
+++ app/config/config.js
@@ -1,1 +1,1 @@
-const baseApiUrl = 'http://104.236.94.240:8080/Travel_ERP/';
+const baseApiUrl = 'http://107.170.103.171:8080/Travel_ERP/';
--- app/views/login/loginCtrl.js
+++ app/views/login/loginCtrl.js
@@ -1,9 +1,10 @@
-angular.module('sbAdminApp').controller('Login', function ($scope,Navigation,Authenticate,API) {
+angular.module('sbAdminApp').controller('Login', function ($scope,Navigation,Authenticate,API,$state) {
     $scope.Login = function(data){
-       API.post('employee/login',data).then(function(response){
+        API.post('employee/login',data).then(function(response){
             Authenticate.doLogin('',response.data.erp_emp_id,response.data.erp_emp_name);
+            $state.go('leads.all', { objId: 479 });
         });
-      //   Authenticate.doLogin('',9,'Rashid');
+        //Authenticate.doLogin('',9,'Rashid');
     };
     $scope.Navigation = Navigation;
     $scope.Authenticate = Authenticate;
