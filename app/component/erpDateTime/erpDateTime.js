541cffc17a143652b38070aa6067ecd3532166f8
Manpreet <manpreet3289@gmail.com>
2016-06-05 00:18:50 +0530

    user cant choose older date than today --- done

--- app/component/erpDateTime/erpDateTime.js
+++ app/component/erpDateTime/erpDateTime.js
@@ -17,6 +17,15 @@ angular.module('sbAdminApp').directive('erpDateTime',function($compile){
     $scope.$watch('data.dateTime',function(data){
         $scope.data.model = $filter('date')(data, format);
     },true)
+
+    $scope.beforeRender = function ($view, $dates, $leftDate, $upDate, $rightDate) {
+        var activeDate = moment(new Date()).subtract(1, $view).add(1, 'minute');
+            for (var i = 0; i < $dates.length; i++) {
+            if ($dates[i].localDateValue() <= activeDate.valueOf()) {
+              $dates[i].selectable = false;
+            }
+        }
+    }
   }
   function link($scope,element,attr){
     element.html('').append($compile(renderHTML())($scope));
@@ -29,7 +38,7 @@ angular.module('sbAdminApp').directive('erpDateTime',function($compile){
                 '</div>'+
               '</a>'+
               '<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">'+
-                '<datetimepicker  ng-model="data.dateTime" data-datetimepicker-config="{dropdownSelector: dateId , minuteStep : data.minuteStep}"/>'+
+                '<datetimepicker  data-before-render="beforeRender($view, $dates, $leftDate, $upDate, $rightDate)" ng-model="data.dateTime" data-datetimepicker-config="{dropdownSelector: dateId , minuteStep : data.minuteStep ,parseFormat :format}"/>'+
               '</ul>'+
         '</div>';
     return html;
