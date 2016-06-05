cd5b76b81a4b43cffb10f65c87d8e4fce3426277
Manpreet <manpreet3289@gmail.com>
2016-06-05 16:15:23 +0530

    modifications

--- app/views/booking/bookingCtrl.js
+++ app/views/booking/bookingCtrl.js
@@ -27,8 +27,10 @@ angular.module('sbAdminApp').controller('BookingCtrl', function($scope,BookingSe
         ,opts : {
                 locale: {format: 'DD/MM/YYYY'},
                 ranges: {
-                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                    'Last 30 Days': [moment().subtract(29, 'days'), moment()]
+                    'Last 7 Days'   : [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days'  : [moment().subtract(29, 'days'), moment()],
+                    'This Month'    : [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month'    : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
                      }
                 }
          ,date : {
