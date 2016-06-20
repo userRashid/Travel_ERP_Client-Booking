8e720b4fba882dd1764df41f4302c441ace9dbd7
Rashid <user.rashid@gmail.com>
2016-06-20 22:34:02 +0530

    - fix a problem

--- app/views/bookingDetail/bookingDetailCtrl.js
+++ app/views/bookingDetail/bookingDetailCtrl.js
@@ -17,12 +17,13 @@ angular.module('sbAdminApp').controller('bookingDetailCtrl', function($scope,$st
           controller    : function($scope,ErpNodeServices,FormData,$uibModalInstance,LeadsServices,Watch){
             $scope.BookingDetail = ErpNodeServices.createForm(FormData.editBookingData());
             $scope.BookingDetail.promise.then(function(data){
-                $scope.bookingButton = Watch.validation(data);
+                 $scope.bookingButton = Watch.validation(data);
+                console.log(' ----- one ',data.data ,modelData);
                 modelData.erp_createdById = ErpNodeServices.getName(modelData.erp_createdById);
                 if(modelData.hasOwnProperty('erp_salesPersonId')) modelData.erp_salesPersonId = modelData.erp_salesPerson.erp_emp_name//GlobalData.getEmployee(modelData.erp_salesPersonId);
                 data.setModel(modelData);
                 $scope._data = data.data;
-                //Watch.makeActualCost(data.data);
+                Watch.makeActualCost(data.data);
             });
           $scope.$watch('_data',function(data){
             Watch.makeActualCost(data);
@@ -33,7 +34,7 @@ angular.module('sbAdminApp').controller('bookingDetailCtrl', function($scope,$st
            $scope.BookingDetail.promise.then(function(data){
              $scope.Model = data.getModel();
              $scope.Model.erp_createdById = Authenticate.user().id;
-            API.put('booking/'+modelData.erp_bookingId,$scope._d).then(function(response){
+            API.put('booking/'+modelData.erp_bookingId,$scope.Model).then(function(response){
                Notify.add('success','Success',response.data.message);
                //LeadsServices.saveLead(leadId,LeadStatus);
                $uibModalInstance.dismiss('cancel');
