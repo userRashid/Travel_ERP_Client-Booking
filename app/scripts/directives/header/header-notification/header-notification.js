c30d6970812882a8ab452e74f74cc38b788f927e
Rashid <user.rashid@gmail.com>
2017-04-06 01:23:26 +0530

    - added functionality for logout

--- app/scripts/directives/header/header-notification/header-notification.js
+++ app/scripts/directives/header/header-notification/header-notification.js
@@ -7,20 +7,20 @@
  * # adminPosHeader
  */
 angular.module('erp')
-	.directive('headerNotification',function(Authenticate,Session){
+	.directive('headerNotification', function (Authenticate, Session) {
 		return {
-            templateUrl:'scripts/directives/header/header-notification/header-notification.html',
-            restrict: 'E',
-            replace: true,
-            controller  :    controller
-    	}
-    	function controller($scope,$state){
-            $scope.userName = Session.get('name');
-    	    $scope.logout = function(){
-    	        Authenticate.doLogout();
-    	        $state.go('leads.home');
-    	    }
-    	}
+			templateUrl: 'scripts/directives/header/header-notification/header-notification.html',
+			restrict: 'E',
+			replace: true,
+			controller: controller
+		}
+		function controller($scope, $state) {
+			$scope.userName = Session.get('name');
+			$scope.logout = function () {
+				Authenticate.doLogout();
+				$state.go('leads.home');
+			}
+		}
 	});
 
 
--- app/scripts/directives/header/header.js
+++ app/scripts/directives/header/header.js
@@ -11,9 +11,31 @@ angular.module('erp')
 		return {
 			templateUrl: 'scripts/directives/header/header.html',
 			restrict: 'E',
-			replace: true
+			replace: true,
+			controller: controller
 		}
+		function controller($scope, API, Session, Authenticate) {
+			$scope.passwordValidate = function (passwordData) {
+				if (!passwordData) return true;
+				var old = passwordData.hasOwnProperty('oldpassword') && passwordData.oldpassword != undefined && passwordData.oldpassword != '';
+				var _new = passwordData.hasOwnProperty('newpassword') && passwordData.newpassword != undefined && passwordData.newpassword != '';
+				var confirm = passwordData.hasOwnProperty('confirmpassword') && passwordData.confirmpassword != undefined && passwordData.confirmpassword != '';
+				var isDisabled = old && _new && confirm &&
+					passwordData.newpassword == passwordData.confirmpassword;
+				return !isDisabled;
+			}
 
+			$scope.changePassword = function (data) {
+				var employeeId = Session.get('id');
+				var temp = new Object();
+				temp.oldPassword = data.oldpassword;
+				temp.newPassword = data.newpassword;
+				API.put('employee/' + employeeId + '/changePassword', temp).then(function (response) {
+					console.log(' ===== ', response);
+					Authenticate.doLogout();
+				});
+			}
+		}
 	});
 
 
