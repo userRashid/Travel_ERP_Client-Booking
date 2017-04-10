77c23e4d41a7ade56b38679ad0943e42d9c97f3e
Rashid <user.rashid@gmail.com>
2017-04-11 00:48:59 +0530

    - validation added on change password

--- app/scripts/directives/header/header.js
+++ app/scripts/directives/header/header.js
@@ -39,3 +39,17 @@ angular.module('erp')
 	});
 
 
+angular.module('erp')
+	.directive('validPassword', function () {
+		return {
+			require: 'ngModel',
+			link: function (scope, elm, attrs, ctrl) {
+				ctrl.$parsers.unshift(function (viewValue, $scope) {
+					var noMatch = viewValue != scope.login.newPassword.$viewValue
+					ctrl.$setValidity('noMatch', !noMatch)
+				})
+			}
+		}
+	});
+
+
