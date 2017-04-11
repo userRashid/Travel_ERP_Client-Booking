e91d5db89a25b3ee006a85bb35fae3d6f00e4589
Rashid <user.rashid@gmail.com>
2017-04-11 15:48:37 +0530

    - make some changes into changePassword

--- app/scripts/directives/header/header.js
+++ app/scripts/directives/header/header.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc directive
- * @name izzyposWebApp.directive:adminPosHeader
- * @description
- * # adminPosHeader
- */
 angular.module('erp')
 	.directive('header', function () {
 		return {
@@ -14,12 +8,15 @@ angular.module('erp')
 			replace: true,
 			controller: controller
 		}
-		function controller($scope, API, Session, Authenticate) {
+		function controller($scope, API, Session, Notify) {
+			$scope.passwordNotMatch = false;
 			$scope.passwordValidate = function (passwordData) {
 				if (!passwordData) return true;
 				var old = passwordData.hasOwnProperty('oldpassword') && passwordData.oldpassword != undefined && passwordData.oldpassword != '';
 				var _new = passwordData.hasOwnProperty('newpassword') && passwordData.newpassword != undefined && passwordData.newpassword != '';
 				var confirm = passwordData.hasOwnProperty('confirmpassword') && passwordData.confirmpassword != undefined && passwordData.confirmpassword != '';
+				$scope.passwordNotMatch = passwordData.newpassword != passwordData.confirmpassword && confirm;
+				$scope.old_new = old && confirm && passwordData.newpassword == passwordData.oldpassword;
 				var isDisabled = old && _new && confirm &&
 					passwordData.newpassword == passwordData.confirmpassword;
 				return !isDisabled;
@@ -31,23 +28,8 @@ angular.module('erp')
 				temp.oldPassword = data.oldpassword;
 				temp.newPassword = data.newpassword;
 				API.put('employee/' + employeeId + '/changePassword', temp).then(function (response) {
-					console.log(' ===== ', response);
-					Authenticate.doLogout();
+					Notify.add('success', 'Password Change', response.data.message);
 				});
-			}
-		}
-	});
-
-
-angular.module('erp')
-	.directive('validPassword', function () {
-		return {
-			require: 'ngModel',
-			link: function (scope, elm, attrs, ctrl) {
-				ctrl.$parsers.unshift(function (viewValue, $scope) {
-					var noMatch = viewValue != scope.login.newPassword.$viewValue
-					ctrl.$setValidity('noMatch', !noMatch)
-				})
 			}
 		}
 	});
